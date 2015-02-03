bc1bf81cf953b54c156e77bc506d4c86e98bd9ea
Scott Nelson <scottnelson@Scotts-MacBook-Pro.local>
2015-02-03 15:28:01 -0800

    not use coffeescript

--- Redly/app/assets/javascripts/application.js
+++ Redly/app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require foundation
 //= require turbolinks
+//= require backbone
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
---
+++ Redly/app/assets/javascripts/redly.js
@@ -0,0 +1,12 @@
+window.Redly = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  Utils: {},
+  initialize: function() {
+    alert("Test Start");
+    new Redly.Routers.Router
+    Backbone.history.start();
+  }
+};
