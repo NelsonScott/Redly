34962d07bbddf940e77b9c35649aed8ab68c3898
Scott Nelson <Scott.D.Nelson@dartmouth.edu>
2015-02-07 19:08:04 -0800

    add user ref to ratings model

---
+++ db/migrate/20150208030609_addusercol.rb
@@ -0,0 +1,5 @@
+class Addusercol < ActiveRecord::Migration
+  def change
+    add_column :ratings, :user_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150208010630) do
+ActiveRecord::Schema.define(version: 20150208030609) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20150208010630) do
     t.integer  "entry_id",   null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "user_feeds", force: true do |t|
