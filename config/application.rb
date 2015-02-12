2f0c67418c6e417959db62ba662dd978912b0421
Scott Nelson <Scott.D.Nelson@dartmouth.edu>
2015-02-12 09:29:54 -0800

    init on precompile fix

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Redly
   class Application < Rails::Application
     config.assets.paths << Rails.root.join("vendor", "assets", "images")
     config.assets.paths << Rails.root.join("vendor", "assets", "fonts")
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
